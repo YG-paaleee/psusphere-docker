b83162efc5de1b1f50cc04353e464b7f32b4334c
YG-paaleee <202380099@psu.palawan.edu.ph>
2025-11-08 10:41:34 +0800

    setup docker

---
+++ README.md
@@ -0,0 +1,50 @@
+# PSUSphere
+> a django web application for managing student organizations, their members, and associated university data at Palawan State University.
+
+## FEATURES
+- Relational Database Schema
+- admin CRUD Interface
+- Custom Data Generation Command
+- Enhanced Admin Views
+- Dependency Management
+
+
+## 🐳 Run with Docker
+To run PSUSphere using Docker:
+
+```bash
+# 1. Clone this repository
+git clone https://github.com/<yourusername>/psusphere-docker.git
+cd psusphere-docker/for_client
+
+# 2. Start the container
+docker compose up
+
+# 3. Open your browser and go to:
+http://localhost:8000
+
+# 4. To stop the container:
+docker compose down
+```
+
+Or pull the image directly:
+```bash
+docker pull 0xe14y3/psusphere:latest
+```
+
+---
+
+## AUTHORS
+<img src="https://avatars.githubusercontent.com/u/225259706?v=4"  style="border-radius: 50%" width=150>
+
+**Name: Ralph Angelo Badang**  
+**Email: Ralphangelobadang06@icloud.com**
+<br>
+<a href="https://www.facebook.com/RalphAngeloBadangg/"><img src="https://github.com/gauravghongde/social-icons/blob/master/PNG/Color/Facebook.png?raw=true" width="40"></a>   <a href="https://github.com/YG-paaleee"><img src="https://github.com/gauravghongde/social-icons/blob/master/PNG/Color/Github.png?raw=true" width="40"></a>
+
+<img src="https://avatars.githubusercontent.com/u/178247970?v=4"  style="border-radius: 50%" width=150>
+
+**Name: Jon Richmond Vitug**  
+**Email: streexs@icloud.com**
+<br>    
+<a href="https://github.com/YG-paaleee"><img src="https://github.com/gauravghongde/social-icons/blob/master/PNG/Color/Facebook.png?raw=true" width="40"></a>   <a href="https://www.facebook.com/exruc"><img src="https://github.com/gauravghongde/social-icons/blob/master/PNG/Color/Github.png?raw=true" width="40"></a>
---
+++ projectsite/static/js/serviceworker.js
@@ -0,0 +1,90 @@
+const CACHE_NAME = 'psusphere-cache-v2';
+const urlsToCache = [
+    '/',
+    '/static/css/sb-admin-2.min.css',
+    '/static/vendor/fontawesome-free/css/all.min.css',
+    '/static/vendor/bootstrap/css/bootstrap.min.css',
+    '/static/vendor/jquery/jquery.min.js',
+    '/static/vendor/bootstrap/js/bootstrap.bundle.min.js',
+    '/static/js/sb-admin-2.min.js',
+    '/static/img/pwa_picture.png',
+];
+
+// Install event - cache resources
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting();
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                // Clone the request
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        // Check if valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                ).catch(function() {
+                    // Return offline fallback page if available
+                    return caches.match('/offline/');
+                });
+            })
+    );
+});
+
+// Background sync for offline functionality
+self.addEventListener('sync', function(event) {
+    if (event.tag === 'sync-data') {
+        event.waitUntil(syncData());
+    }
+});
+
+function syncData() {
+    // Implement data synchronization logic here
+    console.log('Syncing data...');
+}
